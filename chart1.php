912473b3260668b9410547b8119020d59e319be2
Nathan Stanford <Nathan.Stanford@gmail.com>
2022-09-26 23:09:05 -0400

    Example Chart 1

--- chart1.php
+++ chart1.php
@@ -1,7 +1,62 @@
-//in terminal: 
-yarn add chart.js // or npm install chart.js if you use npm
-//html file: 
-<canvas id="my-chart" width="400" height="400"></canvas>
-//js file
-import Chart from 'chart.js'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>chart 1</title>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+  <canvas id="myChart"></canvas>
+</div>
+ 
 
+<script>
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+      label: 'My First dataset',
+      backgroundColor: '#996633',
+      borderColor: '#996633',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+    {
+      label: 'My Second dataset',
+      backgroundColor: '#336699',
+      borderColor: '#336699',
+      data: [10, 15, 7, 9, 60, 50, 85],
+    }
+  ]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+</script>
+
+<script>
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+</script>
+ 
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
